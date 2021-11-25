ec47dc95ab9790dd0088c9a58277e1e04eb127e1
naldiocj <naldiocj@gmail.com>
2021-11-25 22:30:17 +0100

    Add Quiz controller

---
+++ MatData/Controllers/QuizController.cs
@@ -0,0 +1,45 @@
+﻿using MatData.Services.Category;
+using MatData.Services.Municipe;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MatData.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ILogger<CategoryController> _logger;
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ILogger<CategoryController> logger, ICategoryService categoryService)
+        {
+            _logger = logger;
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            _logger.LogInformation("Getting categories");
+
+            return Ok(_categoryService.GetAllCategories());
+        }
+
+        [HttpGet("{categoryId}", Name="ThemesByCategoryId")]
+        public IActionResult GetThemesByCategoryId(int categoryId)
+        {
+            _logger.LogInformation("Getting themes by category id");
+
+            return Ok(_categoryService.GetThemesByCategoryId(categoryId));
+        }
+
+        [HttpGet("{themeId}", Name = "ThemesByCategoryId")]
+        public IActionResult GetThemesByCategoryId(int categoryId)
+        {
+            _logger.LogInformation("Getting themes by category id");
+
+            return Ok(_categoryService.GetThemesByCategoryId(categoryId));
+        }
+    }
+}
