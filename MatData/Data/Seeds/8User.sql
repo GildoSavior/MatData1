0e7785c48a75f1c258a922fabef6a01333a96252
naldiocj <naldiocj@gmail.com>
2022-02-02 12:49:46 +0100

    Add user seed

---
+++ MatData/Data/Seeds/8User.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO "Users" ("Username", "Password", "Role", "Email", "MunicipeId", "Name", "PasswordConfirm", "PhoneNumber", "ProvinceId", "UserType")
+VALUES ('naldiocj@gmail.com', '12345678', 'ADMIN', 'naldiocj@gmail.com', 92, 'Naldio Joaquim', '12345678', '948145100', 11, 'GEPE');
